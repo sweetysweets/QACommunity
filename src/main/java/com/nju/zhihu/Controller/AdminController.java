28378ca5455d4f3fcf79a68160577fcf333b3db9
alwaysbest <106316276@qq.com>
2018-12-15 22:45:35 +0800

    adminController

---
+++ src/main/java/com/nju/zhihu/Controller/AdminController.java
@@ -0,0 +1,35 @@
+package com.nju.zhihu.Controller;
+
+import com.nju.zhihu.Dao.AdminDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.nju.zhihu.Entity.*;
+@RestController
+@RequestMapping(value = "/admin")
+public class AdminController {
+    @Autowired(required = false)
+    private AdminDao adminDao;
+    @RequestMapping(value = "/login")
+    public int getLogin(@RequestParam("aid")String aid,@RequestParam("password")String password){
+        int id = Integer.parseInt(aid);
+        Admin admin = adminDao.getUserById(id);
+        if (admin == null) {
+            return 2;
+        }else{
+            String pass = admin.getPassword();
+            if (pass.equals(password)){
+                return 0;
+            }else {
+                return 1;
+            }
+        }
+
+
+    }
+    @RequestMapping(value = "/deletequestion")
+    public int deleteQuestion(@RequestParam("qid")String qid){
+        return 0;
+    }
+}
