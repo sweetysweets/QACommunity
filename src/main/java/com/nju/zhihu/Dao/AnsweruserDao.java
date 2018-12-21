c059f97363986c584ba46ea7b12f790b4f635bfd
fengxuegt <fengxuegt@163.com>
2018-12-21 14:18:09 +0800

    lw avater

--- src/main/java/com/nju/zhihu/Dao/AnsweruserDao.java
+++ src/main/java/com/nju/zhihu/Dao/AnsweruserDao.java
@@ -3,8 +3,11 @@ package com.nju.zhihu.Dao;
 import com.nju.zhihu.Entity.Answeruser;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface AnsweruserDao {
     public Answeruser getAnsweruser(int answer_id);
+    public List<Answeruser> getAnswerusers(int question_id);
 
 }
